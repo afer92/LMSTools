99c50bfa1425b6a2f546aeaceeaf782f78f5d559
Alain FERRARO <11302604+afer92@users.noreply.github.com>
2021-02-19 22:22:54 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+        name = 'LMSTools',
+        version = '0.1.4',
+        author = 'elParaguayo, dalgwen',
+        author_email = '',
+        url = 'http://github.com/dalgwen/LMSTools',
+        keywords = 'LMS',
+        description = 'A python library for iteracting with your Logitech Media Server. FORKED from elParaguayo/LMSTools',
+        license = 'GNU General Public License (GPL)',
+        packages = ['LMSTools'],
+        install_requires = [],
+
+        classifiers=[
+                'Development Status :: 5 - Production/Stable',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: GNU General Public License (GPL)',
+                'Topic :: Software Development',
+                'Topic :: Software Development :: Libraries',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+        ],
+)
